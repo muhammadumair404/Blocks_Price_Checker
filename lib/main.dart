5dca2f5829e270037e0e78af98c38bdec0779643
Ahsan Arshad <88687816+Ahsanarsha@users.noreply.github.com>
2024-11-06 20:41:18 +0500

    app logo, name, splash added

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Future.delayed(const Duration(seconds: 2)); // Set delay duration
   runApp(
     MultiProvider(
       providers: [
